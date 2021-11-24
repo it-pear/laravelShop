a29b181efaa4772b77ff023daba9f860350b75ab
itpear <yurecbilnovgelarm@gmail.com>
2021-11-25 01:16:55 +0300

    отправка заказа

--- resources/views/mail/orderCreated.blade.php
+++ resources/views/mail/orderCreated.blade.php
@@ -12,16 +12,28 @@
     <h2 style="margin: 3px 0;">Имя: {{ $data[0] }}</h2>
   </div>
   <div style="display: flex;">
-    <h2 style="margin: 3px 0;">Имя: {{ $data[1] }}</h2>
+    <h2 style="margin: 3px 0;">Номер: {{ $data[1] }}</h2>
   </div>
   <div style="display: flex;">
-    <h2 style="margin: 3px 0;">Имя: {{ $data[2] }}</h2>
+    <h2 style="margin: 3px 0;">Субъект страны: {{ $data[2] }}</h2>
   </div>
   <div style="display: flex;">
-    <h2 style="margin: 3px 0;">Имя: {{ $data[3] }}</h2>
+    <h2 style="margin: 3px 0;">Город: {{ $data[3] }}</h2>
   </div>
   <div style="display: flex;">
-    <h2 style="margin: 3px 0;">Имя: {{ $data[4] }}</h2>
+    <h2 style="margin: 3px 0;">Улица: {{ $data[4] }}</h2>
+  </div>
+  <div style="display: flex;">
+    <h2 style="margin: 3px 0;">Дом: {{ $data[5] }}</h2>
+  </div>
+  <div style="display: flex;">
+    <h2 style="margin: 3px 0;">Индекс: {{ $data[6] }}</h2>
+  </div>
+  <div style="display: flex;">
+    <h2 style="margin: 3px 0;">Сообщение: {{ $data[7] }}</h2>
+  </div>
+  <div style="display: flex;">
+    <h2 style="margin: 3px 0;">Способ доставки: {{ $data[8] }}</h2>
   </div>
 
 </body>
